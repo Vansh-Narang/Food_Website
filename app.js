00794696143b6318d193d351621745f2b8b490f7
Vansh=Narang <vanshnarang49906@gmail.com>
2023-02-25 18:13:18 +0530

    Quantity Control

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const express = require('express')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express()
 const path = require('path')
 const port = 3000
 app.use(express.static('public'))
-app.post('/login', function (req, res) {
-    console.log("Login");
-    res.redirect('/')
-})
-// function Ordered() {
-//     let quantity = document.getElementById("Name").innerText
-//     console.log(quantity);
-// }
-app.post('/Addcart', function (req, res) {
-    console.log("Order");
-    res.redirect('/')
-})
-// app.post('/Addcart', function (req, res) {
-//     console.log("Carted");
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.post('/login', (req, res) => {
+//     console.log("Login");
 //     res.redirect('/')
 // })
-app.get('/', function (req, res) {
-    res.send("hello")
+app.post('/', function (req, res) {
+    let value = Number(req.body.Quantity)
+    value = value * 35
+    console.log(value);
+    res.send("Order Your Food For ? : $" + value)
+})
+// app.post('/Addcart', (req, res) => {
+//     console.log("Order");
+//     res.redirect('/')
+// })
+app.get('/', (req, res) => {
+    res.sendFile(__dirname, "/public/index.html")
 });
 app.listen(port, () => {
     console.log("listening");
